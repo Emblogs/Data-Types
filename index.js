3601f16ea3647c41977444e11e43ef7fd76024e7
Emblogs <Emzy45cool@gmail.com>
2022-07-21 05:51:37 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("hello")
+
+let name="dog"
+let pet="Bingo"
+let petname="Dog"+" "+"Bingo"
+let word=2
+let red=2
+let wordred=2+2
+let x=4
+let y=8
+let subtract=y-y
+let divide=y/x
+let multiply=x*y
+let equals=x!=y
+let comparison=x<y
+console.log(comparison)
+console.log(multiply)
+console.log(wordred)
+console.log("name")
+console.log(petname)
