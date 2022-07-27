c222c1c49a209187ce68a58833cf32f2e652306b
Emblogs <Emzy45cool@gmail.com>
2022-07-27 03:41:13 -0700

    modified

--- index.js
+++ index.js
@@ -13,10 +13,24 @@ let divide=y/x
 let multiply=x*y
 let equals=x!=y
 let comparison=x<y
-let numbers=[2,4,6]
+let numbers=[1,2,3]
+let randomarray=[1,"best",true,null]
+let newarray=[5,6,1,2,5,6,7,8,10,100,0,13]
+let lastposition=newarray.length-1
+let object={
+    name:"Emmanuel Chukwuneta",
+    age:15,
+    job:"Game Developer",
+    isRich:true
+}
 console.log(comparison)
-console.log(multiply)
 console.log(wordred)
 console.log("name")
 console.log(petname)
-console.log(numbers)
+console.log(numbers.length)
+console.log(randomarray.length)
+console.log(newarray[lastposition])
+console.log(object)
+console.log(object.name)
+console.log(object["age"])
+console.log(object["job"])
